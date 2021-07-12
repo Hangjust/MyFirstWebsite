674956563ad82a725fb7690a2f0396947514861e
Hangjust <87064605+Hangjust@users.noreply.github.com>
2021-07-12 11:10:12 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,1 @@
-// make a random circle with random color html 5 svg
 
-var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-svg.setAttribute('width', '35%');
-svg.setAttribute('height', '35%');
-svg.setAttribute('viewBox', '0 0 100 100');
-
-var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-circle.setAttribute('cx', 50);
-circle.setAttribute('cy', 50);
-circle.setAttribute('r', 50);
-circle.setAttribute('fill', 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')');
-
-svg.appendChild(circle);
-
-document.body.appendChild(svg);
