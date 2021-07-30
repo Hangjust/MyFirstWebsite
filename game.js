ee744ff83bbff22b9a2155cd1af283afa73db33c
Hangjust <87064605+Hangjust@users.noreply.github.com>
2021-07-30 18:37:16 +0200

    update game.js

--- game.js
+++ game.js
@@ -2,11 +2,30 @@ let lastRenderTime = 0
 const SNAKE_SPEED = 2
 
 function main(currentTime) {
+    window.requestAnimationFrame(main)
   const secondSienceLastRender = (currentTime - lastRenderTime) /1000
-  if secondSienceLastRender < 1 / SNAKE_SPEED 
-  window.requestAnimationFrame(main)
+  if secondSienceLastRender < 1 / SNAKE_SPEED return
+  
+  
+  
+  
+  
   lastRenderTime = currentTime
   console.log(Render)
+  
+  update ()
+  draw ()
+  
 }
 
   window.requestAnimationFrame(main)
+
+
+  function update () {
+    
+  }
+  
+  
+  function draw () {
+    
+  }
