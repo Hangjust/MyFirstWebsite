f5a2d176713f03e52e5dec334099e2141cafe1d3
Hangjust <87064605+Hangjust@users.noreply.github.com>
2021-09-17 13:59:08 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -9,7 +9,7 @@ export function update() {
 
   const inputDirection = getInputDirection()
   for (let i = snakeBody.length - 2; i >= 0; i--) {
-    snakeBody[i + 1000] = { ...snakeBody[i] }
+    snakeBody[i + 1] = { ...snakeBody[i] }
   }
 
   snakeBody[0].x += inputDirection.x
